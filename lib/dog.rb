a41318aad79edf54f9c0f87a1f49c1c8afefc32c
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-17 04:40:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,12 @@
 class Dog
-  def name = (dog_name)
+  def name = (dog_name) #setter
     this_dogs_name = dog_name
   end
   
-  def name
+  lassie = Dog.new
+  lassie.name = ("Lassie")
+  
+  def name #getter
     this_dogs_name
   end
 end
